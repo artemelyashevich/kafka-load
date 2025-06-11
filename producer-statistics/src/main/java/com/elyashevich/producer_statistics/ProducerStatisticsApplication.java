29cbf08634f9ab135942cadb4af893328b458de5
artemelyashevich <elyashevich.artem06@gmail.com>
2025-06-11 15:56:38 +0300

    Add: producer-statistics service

---
+++ producer-statistics/src/main/java/com/elyashevich/producer_statistics/ProducerStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.elyashevich.producer_statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerStatisticsApplication.class, args);
+	}
+
+}
---
+++ producer-statistics/src/test/java/com/elyashevich/producer_statistics/ProducerStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elyashevich.producer_statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerStatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
